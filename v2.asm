34e11a49433621170ccb275a565d42f1a44699b2
Bryk Mykhailo <m.bryk@ukma.edu.ua>
2024-03-26 22:11:57 +0200

    Додав баблсорт, але видалив конвертування. Маю почати з початку і переробити його повністю. Також треба доробити конвертування з аскі у слово (поки що тільки 0-9)

--- v2.asm
+++ v2.asm
@@ -1,10 +1,14 @@
 DATA SEGMENT PARA PUBLIC 'DATA' ; Start of data segment declaration
-MAXLEN DB 20 ; Declare a byte variable MAXLEN and initialize it to 20
+MAXLEN DB 50 ; Declare a byte variable MAXLEN and initialize it to 20
 LEN DB 0 ; Declare a byte variable LEN and initialize it to 0
-MSG DB 30 DUP(?) ; Declare a byte array MSG of size 20, uninitialized
+MSG DB 700 DUP(?) ; Declare a byte array MSG of size 20, uninitialized
 BIN DB 16 DUP(0) ; Define BIN as a 16-byte array
+BinaryArray DB 16 DUP(?) ; Declare an array of 16 bytes
+count DW ? ; Declare a word variable count
 DATA ENDS ; End of data segment declaration
 CODE SEGMENT PARA PUBLIC 'CODE' ; Start of code segment declaration
+
+
 START PROC FAR ; Start of procedure START
 ASSUME CS:CODE, DS:DATA ; Assume that CS points to CODE and DS points to DATA
 PUSH DS ; Push DS onto the stack
@@ -12,14 +16,14 @@ XOR AX, AX ; Clear AX register
 PUSH AX ; Push AX onto the stack
 MOV AX,DATA ; Move the offset of DATA to AX
 MOV DS, AX ; Move the value of AX to DS
- CALL ReadInput; Call the method to read input from the keyboard
- ; Call the method to convert the input to binary
-CALL OutputNewLine ; Call the method to output a newline character
+ CALL ReadInputAsWords; Call the method to read input from the keyboard
 
 CALL AppendNullTerminator ; Call the method to append a null terminator to the input string
-CALL ConvertToNumber ; Call the method to convert the input string to a number
- CALL ConvertToBinary ; Call the method to convert the number to binary
- ; Call the method to output the string
+CALL SortArray ; Call the method to sort the array
+CALL OutputString ; Call the method to output the sorted array
+
+
+
 
 RET ; Return from procedure
 START ENDP ; End of procedure START
@@ -31,39 +35,55 @@ ReadInput PROC NEAR
     RET
 ReadInput ENDP
 
-ConvertToBinary PROC NEAR
-    MOV CX, 16 ; Set the counter to 16 (for 16 bits)
-    MOV BX, OFFSET BIN ; Move the offset of BIN to BX
-    ADD BX, 15 ; Add 15 to BX to start from the end of the array
+SortArray PROC NEAR
+    lea si, MSG
+    mov cx, word ptr count
+    inc cx
+outerLoop:
+    push cx
+    lea si, MSG
+    mov dx, cx ; Copy the outer loop counter to dx for the inner loop
+innerLoop:
+    mov ax, [si]
+    cmp ax, [si+2]
+    jl nextStep
+    xchg [si+2], ax
+    mov [si], ax
+nextStep:
+    add si, 2
+    dec dx ; Decrement dx instead of using loop
+    jnz innerLoop ; Jump to innerLoop if dx is not zero
+    pop cx
+    dec cx ; Decrement cx instead of using loop
+    jnz outerLoop ; Jump to outerLoop if cx is not zero
+RET
+SortArray ENDP
 
-NextBit:
-    MOV DX, AX ; Move the value of AX to DX
-    AND DX, 1 ; Get the least significant bit of DX
-    ADD DL, '0' ; Convert the bit to a character
-    MOV [BX], DL ; Store the character at [BX]
-    SHR AX, 1 ; Shift AX right by 1 bit (equivalent to dividing by 2)
-    DEC BX ; Decrement BX to move to the next character
-    DEC CX ; Decrement the counter
-    JNZ NextBit ; If the counter is not zero, go to the next bit
+ReadInputAsWords PROC NEAR
+    MOV AH, 0AH ; Set AH to 0AH to prepare for interrupt 21H function 0AH (buffered input)
+    MOV DX, OFFSET MAXLEN ; Move the offset of MAXLEN to DX
+    INT 21H ; Call interrupt 21H, function 0AH reads a string from the keyboard
 
+    ; Start of conversion code
+    MOV SI, OFFSET MSG ; Load the address of the input buffer into SI
+    MOV DI, OFFSET BinaryArray ; Load the address of the output buffer into DI
+    XOR CX, CX ; Clear CX to use it as a counter
+
+    ConvertLoop:
+        LODSB ; Load the byte at address SI into AL and increment SI
+        CMP AL, 0 ; Check if we've reached the end of the string
+        JE EndConversion ; If we have, jump to the end of the conversion code
+        SUB AL, '0' ; Convert the ASCII digit to a number
+        MOV [DI], AL ; Store the number in the output buffer
+        INC DI ; Increment DI
+        INC CX ; Increment the counter
+        JMP ConvertLoop ; Repeat the loop
+
+    EndConversion:
+        MOV count, CX ; Update count with the number of elements entered
+    ; Here you can call your sorting algorithm
     RET
-ConvertToBinary ENDP
-
-ConvertToNumber PROC NEAR
-    MOV SI, OFFSET MSG ; Start of the string
-    XOR AX, AX ; Clear AX to store the result
-    XOR BX, BX ; Clear BX to use as a temporary register
-NextDigit:
-    MOV BL, [SI] ; Load the next digit
-    SUB BL, 30H ; Convert from ASCII to number
-   MOV BX, 10 ; Load 10 into BX
-MUL BX ; Multiply AX by BX
-    ADD AX, BX ; Add the new digit
-    INC SI ; Move to the next character
-    CMP BYTE PTR [SI], 0DH ; Check for carriage return (end of input)
-    JNE NextDigit ; If not end of input, process the next digit
-    RET
-ConvertToNumber ENDP
+ReadInputAsWords ENDP
 
 OutputNewLine PROC NEAR
     MOV DL, 10 ; Move 10 to DL
@@ -75,6 +95,8 @@ OutputNewLine PROC NEAR
     RET
 OutputNewLine ENDP
 
+
+
 AppendNullTerminator PROC NEAR
     MOV AL, LEN ; Move the value of LEN to AL
     CBW ; Convert byte to word (sign-extend AL into AX)
@@ -82,25 +104,6 @@ AppendNullTerminator PROC NEAR
     MOV MSG+SI, '$' ; Append a '$' to the end of the string in MSG
     RET
 AppendNullTerminator ENDP
-
-OutputBinary PROC NEAR
-    MOV CX, 16 ; Set the counter to 16 (for 16 bits)
-    MOV BX, OFFSET BIN ; Move the offset of BIN to BX
-    ADD BX, 15 ; Add 15 to BX to start from the end of the array
-
-NextChar:
-    MOV DL, [BX] ; Move the character at [BX] to DL
-    MOV AH, 02H ; Set AH to 02H to prepare for interrupt 21H function 02H (print character)
-    INT 21H ; Call interrupt 21H, function 02H prints a character
-    DEC BX ; Decrement BX to move to the next character
-    DEC CX ; Decrement the counter
-    JNZ NextChar ; If the counter is not zero, go to the next character
-
-    MOV DX, OFFSET MSG ; Move the offset of MSG to DX
-    MOV AH, 09H ; Set AH to 09H to prepare for interrupt 21H function 09H (output string)
-    INT 21H ; Call interrupt 21H, function 09H outputs a string
-    RET
-OutputBinary ENDP
 
 OutputString PROC NEAR
  MOV AH, 09H ; Set AH to 09H to prepare for interrupt 21H function 09H (output string)
